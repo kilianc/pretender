60c6dac74eedfbd8257b8648e2eeb2d55f986736
Kilian Ciuffolo <kciuffolo@roblox.com>
2024-04-12 19:59:35 -0700

    chore: asser dirFS to ReadFileFS

--- internal/pretender/pretender.go
+++ internal/pretender/pretender.go
@@ -2,7 +2,6 @@ package pretender
 
 import (
 	"fmt"
-	"io"
 	"io/fs"
 	"log/slog"
 	"net/http"
@@ -16,7 +15,7 @@ type HttpHandler struct {
 	sync.Mutex
 	index     int
 	responses []string
-	fs        fs.FS
+	fs        fs.ReadFileFS
 	logger    *slog.Logger
 }
 
@@ -28,23 +27,19 @@ func NewHttpHandler(logger *slog.Logger) *HttpHandler {
 
 func (hh *HttpHandler) LoadResponsesFile(name string) error {
 	// this is needed to allow relative paths as input from the CLI
-	absoluteFilePath, err := filepath.Abs(name)
+	fullname, err := filepath.Abs(name)
 	if err != nil {
 		return fmt.Errorf("failed to get absolute path for responses file [%s]: %w", name, err)
 	}
 
 	// HttpHandler.fs is only set in tests, we default to os filesystem
 	if hh.fs == nil {
-		hh.fs = os.DirFS(filepath.Dir(absoluteFilePath))
+		dir := filepath.Dir(fullname)
+		hh.fs = os.DirFS(dir).(fs.ReadFileFS)
 	}
 
-	file, err := hh.fs.Open(filepath.Base(absoluteFilePath))
-	if err != nil {
-		return fmt.Errorf("failed to open responses file [%s]: %w", name, err)
-	}
-	defer file.Close()
-
-	content, err := io.ReadAll(file)
+	basename := filepath.Base(fullname)
+	content, err := hh.fs.ReadFile(basename)
 	if err != nil {
 		return fmt.Errorf("failed to read responses file [%s]: %w", name, err)
 	}
