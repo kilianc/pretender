6026f1898432d86331d9e5d5e00457b095eeaca9
Kilian Ciuffolo <kciuffolo@roblox.com>
2024-04-14 16:43:56 -0700

    chore: bump version to v1.0.3

--- cmd/pretender/main.go
+++ cmd/pretender/main.go
@@ -15,7 +15,7 @@ import (
 	"github.com/lmittmann/tint"
 )
 
-const version = "v1.0.2"
+const version = "v1.0.3"
 
 func main() {
 	responseFileName := flag.String("responses", "responses.txt", "path to the file with responses")
