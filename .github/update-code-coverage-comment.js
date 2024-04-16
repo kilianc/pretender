f7b156f900b0fc0222fde1ddc29f6d624c98d434
Kilian Ciuffolo <385716+kilianc@users.noreply.github.com>
2024-04-16 09:48:10 -0700

    chore: add code coverage (#26)

---
+++ .github/update-code-coverage-comment.js
@@ -0,0 +1,46 @@
+const fs = require('fs')
+
+const updateCodeCoverageComment = module.exports = async ({ context, github }) => {
+  const comments = await github.rest.issues.listComments({
+    owner: context.repo.owner,
+    repo: context.repo.repo,
+    issue_number: context.issue.number,
+    per_page: 100
+  })
+
+  const coverageComment = comments.data.find((comment) => {
+    return comment.body.startsWith('<!-- coverage -->')
+  }) || {}
+
+  const coverageText = fs.readFileSync('coverage-text.txt', 'utf8').split('\n').slice(0, -1)
+  const coverageTextSummary = coverageText[coverageText.length-1].split('\t').pop()
+
+  const commentBody = [
+    '<!-- coverage -->',
+    `### Code Coverage Report ${process.env.REVISION}`,
+    '```',
+    `Total: ${coverageTextSummary}`,
+    '```',
+    '<details>',
+    '<summary>Full coverage report</summary>',
+    '',
+    '```',
+    ...coverageText,
+    '```',
+    '</details>',
+  ].join('\n')
+
+  const upsertCommentOptions = {
+    owner: context.repo.owner,
+    repo: context.repo.repo,
+    issue_number: context.issue.number,
+    comment_id: coverageComment.id,
+    body: commentBody
+  }
+
+  if (coverageComment.id) {
+    await github.rest.issues.updateComment(upsertCommentOptions)
+  } else {
+    await github.rest.issues.createComment(upsertCommentOptions)
+  }
+}
