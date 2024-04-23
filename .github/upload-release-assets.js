4c271bfaf2d60ac3b0746e973f33541c246cd47d
Kilian Ciuffolo <385716+kilianc@users.noreply.github.com>
2024-04-22 17:14:03 -0700

    chore: fix manual trigger for release workflow (#41)

--- .github/upload-release-assets.js
+++ .github/upload-release-assets.js
@@ -1,16 +1,16 @@
 const fs = require('fs')
 
-const uploadReleaseAssets = module.exports = async ({ context, github }) => {
+const uploadReleaseAssets = module.exports = async ({ context, github }, tag) => {
   const files = fs.readdirSync('bin').filter(file => file.endsWith('.tar.gz'))
 
   const release = await github.rest.repos.getReleaseByTag({
     owner: context.repo.owner,
     repo: context.repo.repo,
-    tag: context.ref.replace('refs/tags/', '')
+    tag: tag.replace('refs/tags/', '')
   })
 
   for (const file of files) {
-    console.log(`Uploading bin/${file} to release ${context.ref.replace('refs/tags/', '')} id=${release.data.id}`)
+    console.log(`Uploading bin/${file} to release ${tag.replace('refs/tags/', '')} id=${release.data.id}`)
 
     await github.rest.repos.uploadReleaseAsset({
       owner: context.repo.owner,
