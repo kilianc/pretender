eddf4cdd54dbefc533e2bde1e9d7b9c87322cfb7
Kilian Ciuffolo <kciuffolo@roblox.com>
2024-04-27 18:50:11 -0700

    improved cover check

--- tools/covercheck/main.go
+++ tools/covercheck/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 	"regexp"
+	"strconv"
 )
 
 func main() {
@@ -11,21 +12,27 @@ func main() {
 
 	coverageSummary, err := os.ReadFile("cover.txt")
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "\033[91m✘ error: reading cover.txt: %v\033[0m\n", err)
+		fmt.Fprintf(os.Stderr, "\033[91m✘ error: reading cover.txt: %v\033[0m\n\n", err)
 		os.Exit(1)
 	}
 
-	regexp := regexp.MustCompile(`total:.+(100.0%)`)
+	regexp := regexp.MustCompile(`total:.+\t+(\d?\d?\d.\d)%`)
 
 	match := regexp.FindStringSubmatch(string(coverageSummary))
 
 	if len(match) < 2 {
-		fmt.Fprintf(os.Stderr, "\033[91m✘ error: coverage not found in cover.txt\033[0m\n")
+		fmt.Fprintf(os.Stderr, "\033[91m✘ error: coverage not found in cover.txt\033[0m\n\n")
 		os.Exit(1)
 	}
 
-	if match[1] != "100.0%" {
-		fmt.Fprintf(os.Stderr, "\033[91m✘ error: coverage is below 100%%: %s\033[0m\n", match[1])
+	coverage, err := strconv.ParseFloat(match[1], 64)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "\033[91m✘ error: failed to convert coverage to float: %v\033[0m\n\n", err)
+		os.Exit(1)
+	}
+
+	if coverage != 100.0 {
+		fmt.Fprintf(os.Stderr, "\033[91m✘ error: coverage is below 100%%: %.1f%%\033[0m\n\n", coverage)
 		os.Exit(1)
 	}
 
